7e74a9e6f594d308cd79a0d135c7f106541c2b8f
Travis Gaff <travis@labzero.com>
2023-05-24 14:51:47 +0900

    step 4,5: reference image in JS and load into page
    This works in development just fine.  But sprockets runs development differently than production.

--- app/javascript/controllers/images_controller.js
+++ app/javascript/controllers/images_controller.js
@@ -1,8 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
+import Paint from "../images/paint.png"
 
 // Connects to data-controller="images"
 export default class extends Controller {
   connect() {
     console.log('images controller connected')
+
+    const img = new Image(640, 427)
+    img.src = Paint
+
+    this.element.appendChild(img)
   }
 }
